440765f225e29cf098fdb64e9a7a3f1461c2f9ba
sl.ars <sl.ars@icloud.com>
2025-11-03 12:45:23 +0500

    chore: keep legacy helper files

--- RemoteVersion.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors=yes')
-
-remoteDriver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=options)
